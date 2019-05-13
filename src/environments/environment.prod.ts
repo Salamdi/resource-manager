08704647a2c2aa131f4bb5d35578e11fe01b7c87
Dinislam_Abdulsalamov <Dinislam_Abdulsalamov@epam.com>
2019-05-13 19:48:12 +0300

    build, add 404.html

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
 
 export const authParams = {
   client_id: '569155344129-1p1ek0gd7put5c76aal2blrnoa8ak2pi.apps.googleusercontent.com',
-  redirect_uri: 'salamdi.github.io/resource-manager/auth',
+  redirect_uri: 'https://salamdi.github.io/resource-manager/auth',
   response_type: 'token',
   scope: 'https://www.googleapis.com/auth/spreadsheets.readonly'
 };
